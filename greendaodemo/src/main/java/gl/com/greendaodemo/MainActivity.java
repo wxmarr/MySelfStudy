1d0fae43625c7852297dc03dc2bc4e8e71abf1f3
guolei1130 <1120832563@qq.com>
2015-10-29 19:38:48 +0800

    greenDao 增加多表关联

--- greendaodemo/src/main/java/gl/com/greendaodemo/MainActivity.java
+++ greendaodemo/src/main/java/gl/com/greendaodemo/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dao = initDao();
         initView();
 
-       initColumnsAndData(dao);
+        initColumnsAndData(dao);
 
     }
 
